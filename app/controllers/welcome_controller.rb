97274771e011ad054a851c5741512a6413ca89d5
ef718 <elfang16@gmail.com>
2015-11-11 23:53:59 -0500

    Show tweets

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,20 +4,10 @@ class WelcomeController < ApplicationController
 
   def search
     @search_terms = params[:q].downcase
-    client.get_all_tweets(@search_terms)
+    get_tweets(@search_terms)
   end
 
-  def client.get_all_tweets(user)
-    collect_with_max_id do |max_id|
-      options = {count: 10, include_rts: true}
-      options[:max_id] = max_id unless max_id.nil?
-      user_timeline(user, options)
-    end
-  end
-
-  def collect_with_max_id(collection=[], max_id=nil, &block)
-    response = yield(max_id)
-    collection += response
-    response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
+  def get_tweets(user)
+    @tweets = TwitterApi.client.user_timeline(user)
   end
 end
